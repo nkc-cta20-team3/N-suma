b06d6ee68bed017dc2a29978e973c273b211fb81
yukke23 <ec98721zzz@gmail.com>
2023-05-24 13:52:38 +0900

    complete

--- go/src/main/go.mod
+++ go/src/main/go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/go-xorm/xorm v0.7.9 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
@@ -27,4 +29,6 @@ require (
 	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	xorm.io/builder v0.3.6 // indirect
+	xorm.io/core v0.7.2-0.20190928055935-90aeac8d08eb // indirect
 )
--- go/src/main/main.go
+++ go/src/main/main.go
@@ -1,17 +1,49 @@
 package main 
 
 import (
+    "log"
+
     "github.com/gin-gonic/gin"
+
+    // ローカルモジュールのインポート
+    "main/infra"
+    "main/handler"
+    "main/service"
+
+
 )
 
 func main() {
-    router := gin.Default()
 
-    router.GET("/", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "Hello World",
-        })
-    })
+    //
+	engine := infra.DBInit()
+	factory := service.NewService(engine)
+	defer func() {
+		log.Println("engine closed")
+		engine.Close()
+	}()
 
-    router.Run(":8080")
+    // 
+    g := gin.Default()
+	g.Use(service.ServiceFactoryMiddleware(factory))
+
+    // 
+    routes := g.Group("/v1")
+	{
+        // user
+		routes.POST("/users", handler.Create)
+		routes.GET("/users", handler.GetAll)
+		routes.GET("/users/:user-id", handler.GetOne)
+		routes.PUT("/users/:user-id", handler.Update)
+		routes.DELETE("/users/:user-id", handler.Delete)
+        
+    }
+
+    routes2 := g.Group("/v2")
+    {
+        // hello world
+        routes2.GET("/hello", handler.Hello)
+    }
+
+    g.Run(":8080")
 }
