c3706a98ece3d3f55a3bcad103965162141750f2
Naga564 <yanagayakisoba@gmail.com>
2023-05-26 14:42:43 +0900

    "JSONを受け取って試してみたい"

--- go/src/main/api/UnAuthorizationList.go
+++ go/src/main/api/UnAuthorizationList.go
@@ -1,22 +1,57 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"main/model/???"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Teacher_data struct {
+	teacher_id int    `json:"teacher_id"`
+	position   int    `json:"position"`
+	class_name string `json:"class_name"`
+}
+
 func UnAuthorizationList(c *gin.Context) {
 
-	aaa := c.Param("teacher_data")
+	
+	// aaa := c.Param("teacher_data")
+
+	
+
+	input := model.UserInput{}
+
+	// ヘッダーのJSONをinputにバインドします
+	err := c.ShouldBindWith(&input, binding.JSON)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Bad request")
+		return
+	}
+
+
+
+	var data Teacher_data
+
+	err := json.Unmarshal("teacher_data", &data)
+
+	if err != nil {
+		//エラー
+	}
+
+	fmt.Println(data.teacher_id)
+	fmt.Println(data.position)
+	fmt.Println(data.class_name)
 
 	// JSONに変えるときに使う
-	// payload := gin.H{
-	// 	"message": aaa,
-	// }
+	payload := gin.H{
+		"message": aaa,
+	}
 
 	// ステータス200と、payloadを返します
-	//c.JSON(http.StatusOK, payload)
-	c.JSON(http.StatusOK, aaa)
+	c.JSON(http.StatusOK, payload)
+	// c.JSON(http.StatusOK, aaa)
 
 }
