3ffe484900e82a34b293ca4db43e9619647c5b06
Naga564 <yanagayakisoba@gmail.com>
2023-06-07 14:53:06 +0900

    "勉強しました"

--- go/src/main/api/UnAuthorizationList.go
+++ go/src/main/api/UnAuthorizationList.go
@@ -1,25 +1,37 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
-	// "main/model/???"
-	// "main/infra"
+	"main/infra"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Teacher_data struct {
-	teacher_id int    `json:"teacher_id"`
-	position   int    `json:"position"`
-	class_name string `json:"class_name"`
+type Document2 struct {
+	DocumentID int    `json:"document_id"` // ドキュメントID
+	StudentID  int    `json:"student_id"`  // 学籍番号
+	Location   string `json:"location"`    // 場所
 }
 
 func UnAuthorizationList(c *gin.Context) {
 
-	aaa := c.Param("teacher_data")
-
+	document_id := c.Param("teacher_data")
 	// input := model.UserInput{}
+
+	var document = Document2{}
+	db := infra.DBInitGorm()
+
+	db.Table("absence_document").Select(
+		"document_id",
+		"student_id",
+		"location").Where("document_id > ?", document_id).First(&document)
+	if db.Error != nil {
+		fmt.Print("ERROR!")
+	}
+
+	fmt.Printf("document_id: %d, student_id: %d, location: %s\n", document.DocumentID, document.StudentID, document.Location)
 
 	// ヘッダーのJSONをinputにバインドします
 	// err := c.ShouldBindWith(&input, binding.JSON)
@@ -36,20 +48,17 @@ func UnAuthorizationList(c *gin.Context) {
 	// 	//エラー
 	// }
 
-	// fmt.Println(data.teacher_id)
-	// fmt.Println(data.position)
-	// fmt.Println(data.class_name)
-
 	// JSONに変えるときに使う
-	payload := gin.H{
-		"message": aaa,
-	}
+	/*
+		payload := gin.H{
+			"message": document_id,
+		}*/
 
 	//DB接続
 	// db = infra.DBInit()
 
 	// ステータス200と、payloadを返します
-	c.JSON(http.StatusOK, payload)
+	c.JSON(http.StatusOK, document)
 	// c.JSON(http.StatusOK, aaa)
 
 }
