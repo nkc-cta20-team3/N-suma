b7ae78e8b01c57402b1e0eb609a94b1dbaccd65f
Naga564 <yanagayakisoba@gmail.com>
2023-06-12 14:52:41 +0900

    リファクタリング

--- go/src/main/api/UnAuthorizationList.go
+++ go/src/main/api/UnAuthorizationList.go
@@ -10,36 +10,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Document2 struct {
-	DocumentID int    `json:"document_id"` // ドキュメントID
-	StudentID  int    `json:"student_id"`  // 学籍番号
-	Location   string `json:"location"`    // 場所
-}
-
-// // 引数の構造体
-// type TeacherData struct {
-// 	TeacherID int    `json:"teacher_id"`
-// 	Position  int    `json:"position"`
-// 	ClassName string `json:"class_name"`
-// }
-
-// // 戻り値の構造体
-// type UnAuthorizeList struct {
-// 	ClassName       string `json:"class_name"`       //クラス名称
-// 	StudentName     string `json:"student_name"`     //学生氏名
-// 	AbsenceCategory string `json:"absence_category"` //種別
-// 	DocumentID      int    `json:"document_id"`      //書類ID
-// }
-
 func UnAuthorizationList(c *gin.Context) {
 
 	//引数を取得する
 	status := c.Param("teacher_data")
 
-	// input := model.UserInput{}
-
+	//構造体定義
 	document := []model.UnAuthorizeList{}
-	//document := UnAuthorizeList{}
+	//DB接続
 	db := infra.DBInitGorm()
 
 	db.Table("absence_document AS ad").
@@ -55,36 +33,6 @@ func UnAuthorizationList(c *gin.Context) {
 	if db.Error != nil {
 		fmt.Print("ERROR!")
 	}
-
-	//確認用出力
-	/*
-		fmt.Printf("class_name: %s, student_name: %s, absence_category: %s,document_id: %d\n",
-			document.ClassName, document.StudentName, document.AbsenceCategory, document.DocumentID)
-	*/
-
-	// ヘッダーのJSONをinputにバインドします
-	// err := c.ShouldBindWith(&input, binding.JSON)
-	// if err != nil {
-	// 	c.String(http.StatusBadRequest, "Bad request")
-	// 	return
-	// }
-
-	// var data Teacher_data
-
-	// err := json.Unmarshal("teacher_data", &data)
-
-	// if err != nil {
-	// 	//エラー
-	// }
-
-	// JSONに変えるときに使う
-	/*
-		payload := gin.H{
-			"message": document_id,
-		}*/
-
-	//DB接続
-	// db = infra.DBInit()
 
 	// ステータス200と、payloadを返します
 	c.JSON(http.StatusOK, document)
