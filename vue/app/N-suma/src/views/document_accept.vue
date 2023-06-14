2417229c8de71062a0b4cc522a99c7c395b789ca
masakiwakida <bluekakarotto.7s@ezweb.ne.jp>
2023-06-14 14:12:17 +0900

    三項演算子に(田橋君が)改良しました。

--- vue/app/N-suma/src/views/document_accept.vue
+++ vue/app/N-suma/src/views/document_accept.vue
@@ -20,38 +20,29 @@
                 <input type="text" v-model="inputValue" class="input" style="font-size: 1.25em" placeholder="教員用コメント">
             </div>
             <div class="is-flex mb-5">
-                <div class="ml-4 button is-medium" @click="AcceptanceAlert">受理</div>
-                <div class="ml-4 button is-medium" @click="RejectedAlert">却下</div>
+                <div class="ml-4 button is-medium" @click="ClickAlert(true)">受理</div>
+                <div class="ml-4 button is-medium" @click="ClickAlert(false)">却下</div>
             </div>
         </div>
     </div>
 
 </template>
   
-<script>
-export default {
-    data() {
-        return {
-            inputValue:""
-        };
-    },
-    methods: {
-        AcceptanceAlert() {
-            if (this.inputValue){
-                alert("受理されました");
-            } else {
-                alert("コメントを入力してください。");
-            }      
-        },
-        RejectedAlert() {
-            if (this.inputValue){
-                alert("却下されました");
-            } else {
-                alert("コメントを入力してください。");
-            }
-        }
-    }
-};
+<script setup>
+
+import { ref } from 'vue';
+
+const inputValue = ref('')
+
+const ClickAlert = (isAcceptance) => {
+
+    if (inputValue.value){
+        alert( (isAcceptance ? "受理" : "却下") + "されました");
+    } else {
+        alert("コメントを入力してください。");
+    }      
+}
+
 </script>
 
 
