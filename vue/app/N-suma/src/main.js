ff0575c757c62cd9f373ce51b22b6b09f3a63c72
otake030 <grcrew0817@gmail.com>
2023-06-19 11:06:10 +0900

    storeのimportとstoreにuserとuseridの保存

--- vue/app/N-suma/src/main.js
+++ vue/app/N-suma/src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import { getAuth,onAuthStateChanged } from 'firebase/auth'
 
 const app = createApp(App)
-  .use(router)
-  .mount('#app')
+
+// Firebaseのログイン状態を監視し、VuexストアにユーザーIDを保存する
+onAuthStateChanged(getAuth(), (user) => {
+  if (user) {
+    store.dispatch('updateUserId', user.uid)
+  }
+})
+
+app.use(router).use(store).mount('#app')
