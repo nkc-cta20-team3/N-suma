383f6f988d34f50334ea44d2446f0378421f6bc7
yukke23 <ec98721zzz@gmail.com>
2023-06-20 22:55:32 +0900

    Enable the use of environment variables in connection strings

--- go/src/main/infra/gorm.go
+++ go/src/main/infra/gorm.go
@@ -2,6 +2,7 @@ package infra
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,7 +10,7 @@ import (
 
 func DBInitGorm() *gorm.DB {
 	// データベース接続
-	db, err := gorm.Open(mysql.Open("root:password@tcp(db:3306)/cta20gr3?charset=utf8mb4&parseTime=true"))
+	db, err := gorm.Open(mysql.Open(os.Getenv("DB_HOST")))
 	// データベース接続エラー
 	if err != nil {
 		log.Println(err)
