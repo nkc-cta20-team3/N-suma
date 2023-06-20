3d8669600128cd7679ef9d59b2ab18bd56b8e09a
yukke23 <ec98721zzz@gmail.com>
2023-06-20 14:24:44 +0900

    update store

--- vue/app/N-suma/src/store/index.js
+++ vue/app/N-suma/src/store/index.js
@@ -4,7 +4,8 @@ const store = createStore({
   state() {
     return {
       userId: null,
-      ClassId: null
+      ClassId: null,
+      DocId: null
     }
   },
   mutations: {
@@ -13,12 +14,21 @@ const store = createStore({
     },
     setClassId(state, ClassId) {
       state.ClassId = ClassId
+    },
+    setDocId(state, DocId) {
+      state.DocId = DocId
     }
   },
   actions: {
     updateUserId({ commit }, userId) {
       // ユーザIDをVuexのstateに保存する
       commit('setUserId', userId)
+    },
+    updateClassId({ commit }, ClassId) {
+      commit('setClassId', ClassId)
+    },
+    updateDocId({ commit }, DocId) {
+      commit('setDocId', DocId)
     }
   },
   getters: {
@@ -27,6 +37,9 @@ const store = createStore({
     },
     getClassId(state) {
       return state.ClassId
+    },
+    getDocId(state) {
+      return state.DocId
     }
   }
 })
