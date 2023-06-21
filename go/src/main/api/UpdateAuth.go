5fdca72efaf497a1fc66014872653004f177914b
Naga564 <107987877+Naga564@users.noreply.github.com>
2023-06-21 13:40:19 +0900

    absence update (#98)
    * POST送信に向けてフロントを触った
    
    * POST用にバックエンド側の構造体を修正
    
    * 構造体定義のコメントを修正、わかりやすくした
    
    * practice.vueからAPIをGETとPOSTメソッドで叩けるように更新
    
    * GETメソッドのpayloadを実装、クラス名で絞ることを考えている途中
    
    * クラス名をAPIの引数に組み込まずに実装
    
    * POSTのAPIで、教員IDのみで未認可リストを表示できるように修正
    
    * POSTのAPIの不要なコメントの削除、例外処理(笑)を実装
    
    * GETメソッドの方も引数1つに対応、作業用構造体をabsence.goに移行、伴いPOSTも修正
    
    * update models
    
    * update api routing
    
    * fix routing
    
    * delete old files
    
    * add models
    
    * refactor func
    
    * とりあえずbuildを通した。
    Saveで更新するのではなくUpdateを使って更新する必要がある
    
    * update routing
    
    * create api base
    
    * commentout
    
    * POSTで値を取れるように修正
    
    * UPDATE文を通るようにした
    
    * update error handling
    
    * fix import package
    
    * interruption
    
    * fix ReadDocument
    
    * クラスIDとユーザIDの保存する場所
    
    * add vuex package
    
    * vue developtool update
    
    * storeのimportとstoreにuserとuseridの保存
    
    * ボタン押下時にデータの所得とstoreに送信
    
    * 担任と教員の見分
    
    * router-linkをbuttonに変更
    
    * リストの読み込み処理を修正
    
    * APIを叩いてデータを取得できるようにした!
    
    * ドキュメントIDをstoreに保持してから画面遷移するように更新した
    
    * fix api endpoint
    
    * fix store
    
    * update store
    
    * storeのDocIdを使用して、API経由でデータの取得まで実装
    
    * fix and add model
    
    * fix updateAuth api
    
    * fix syntax updateAuth api
    
    * 書類の認可を実装
    
    * fix UpdateAuth api
    
    * Delete unwanted practice.vue
    
    ---------
    
    Co-authored-by: T_YT <101075832+yukke23@users.noreply.github.com>
    Co-authored-by: yukke23 <ec98721zzz@gmail.com>
    Co-authored-by: otake030 <grcrew0817@gmail.com>
    Co-authored-by: suzukikaito244 <irookhihi@icloud.com>

---
+++ go/src/main/api/UpdateAuth.go
@@ -0,0 +1,70 @@
+package api
+
+import (
+	"log"
+	"net/http"
+
+	"main/infra"
+	"main/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UpdateAuth(c *gin.Context) {
+
+	request := model.UpdateAuthRequest{}
+
+	//POSTで受け取った値を格納する
+	if err := c.ShouldBindJSON(&request); err != nil {
+		// エラーな場合、ステータス400と、エラー情報を返す
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	//DB接続
+	db := infra.DBInitGorm()
+
+	//必要な変数を定義
+	var documentStatus int
+	var teacherPosition int
+	response := http.StatusBadRequest
+
+	//認可ステータスの取得
+	db.Table("absence_document").Select("status").Where("document_id = ?", request.DocumentID).Scan(&documentStatus)
+
+	//役職IDの取得
+	db.Table("teachers").Select("position_id").Where("teacher_id = ?", request.TeacherID).Scan(&teacherPosition)
+
+	log.Println(request)
+	log.Println(documentStatus)
+	log.Println(teacherPosition)
+
+	if teacherPosition == documentStatus+1 {
+		
+		if teacherPosition == 1 {
+			//担任だけが変更する
+			db.Table("absence_document").
+				Where("document_id = ?", request.DocumentID).
+				Updates(model.UpdateDocument{Status: teacherPosition, TeacherComment: request.TeacherComment})
+		}else if teacherPosition >= 2 {
+			//教員が認可する
+			db.Table("absence_document").
+				Where("document_id = ?", request.DocumentID).
+				Updates(model.UpdateDocument{Status: teacherPosition})
+		}
+
+		//エラーハンドリング
+		if db.Error != nil {
+			errMsg := "UPDATE ERROR"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": errMsg})
+			return
+		}
+
+		response = http.StatusOK
+
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": response,
+	})
+}
--- go/src/main/model/absence.go
+++ go/src/main/model/absence.go
@@ -5,7 +5,71 @@ import (
 	"time"
 )
 
-type AbsenceDocument struct {
+// ReadAuthListで使用する構造体
+type ReadAuthListRequest struct {
+	TeacherID int `json:"teacher_id"`
+}
+
+type ReadAuthListResponse struct {
+	ClassName  string `json:"class_name"`
+	StudentName string `json:"student_name"`
+	AbsenceCategory string `json:"absence_category"`
+	DocumentID int `json:"document_id"`
+}
+
+type TakeClassName struct {
+	PositionID int    `json:"position_id"`
+	ClassName  string `json:"class_name"`
+}
+
+
+
+// ReadDocumentで使用する構造体
+type ReadDocumentRequest struct {
+	DocumentID int `json:"document_id"`
+}
+
+type ReadDocumentResponse struct {
+	DocumentID int `json:"document_id"`
+	RequestDate time.Time `json:"request_date"`
+	StudentID int `json:"student_id"`
+	ClassName string `json:"class_name"`
+	StudentName string `json:"student_name"`
+	StartDate time.Time `json:"absence_start_date"`
+	StartFlame int `json:"start_flame"`
+	EndDate time.Time `json:"end_date"`
+	EndFlame int `json:"end_flame"`
+	Location string `json:"location"`
+	StudentComment string `json:"student_comment"`
+	TeacherComment string `json:"teacher_comment"`
+}
+
+// UpdateAuthで使用する構造体
+type UpdateAuthRequest struct {
+	DocumentID int `json:"document_id"`
+	TeacherID int `json:"teacher_id"`
+	TeacherComment string `json:"teacher_comment"`
+}
+
+type UpdateDocument struct {
+	DocumentID        int            `json:"document_id"`         // ドキュメントID
+	StudentID         int            `json:"student_id"`          // 学籍番号
+	CompanyID         int            `json:"company_id"`          // 会社ID
+	ReasonID          int            `json:"reason_id"`           // 理由ID
+	RequestDate       time.Time      `json:"request_date"`        // 申請日
+	AbsenceStartDate  time.Time      `json:"absence_start_date"`  // 欠席開始日
+	AbsenceStartFlame int            `json:"absence_start_flame"` // 欠席開始時限
+	AbsenceEndDate    time.Time      `json:"absence_end_date"`    // 欠席終了日
+	AbsenceEndFlame   int            `json:"absence_end_flame"`   // 欠席終了時限
+	Location          string         `json:"location"`            // 場所
+	ReadFlag          bool           `json:"read_flag"`           // 既読フラグ
+	Status            int            `json:"status"`              // ステータス
+	StudentComment    sql.NullString `json:"student_comment"`     // 学生コメント
+	TeacherComment    string		 `json:"teacher_comment"`     // 教員コメント
+}
+
+// CreateDocumentで使用する構造体
+type CreateDocumentRequest struct {
 	DocumentID        int            `json:"document_id"`         // ドキュメントID
 	StudentID         int            `json:"student_id"`          // 学籍番号
 	CompanyID         int            `json:"company_id"`          // 会社ID
@@ -22,17 +86,7 @@ type AbsenceDocument struct {
 	TeacherComment    sql.NullString `json:"teacher_comment"`     // 教員コメント
 }
 
-// 引数の構造体
-type TeacherData struct {
-	TeacherID int    `json:"teacher_id"` //教員ID
-	Position  int    `json:"position"`   //役職
-	ClassName string `json:"class_name"` //担任クラス名
-}
-
-// 戻り値の構造体
-type UnAuthorizeList struct {
-	ClassName       string `json:"class_name"`       //クラス名称
-	StudentName     string `json:"student_name"`     //学生氏名
-	AbsenceCategory string `json:"absence_category"` //種別
-	DocumentID      int    `json:"document_id"`      //書類ID
+// DeleteDocumentで使用する構造体
+type DeleteDocumentRequest struct {
+	DocumentID int `json:"document_id"`
 }
