6c0a0ccea157bd5f44db90c7e679c23b76004cfb
Naga564 <yanagayakisoba@gmail.com>
2023-06-26 10:26:24 +0900

    testcode修正

--- go/src/main/api/GetPosition.go
+++ go/src/main/api/GetPosition.go
@@ -1,36 +1,42 @@
 package api
 
-import{
+import (
 	"fmt"
+	"net/http"
 
 	"main/infra"
-	"main/model"
-	"github.com/gin-gonic/gin"
-}
 
-type struct UserData {
+	"github.com/gin-gonic/gin"
+)
+
+type UserData struct {
 	UserID int "json/user_id"
 }
 
-type struct UserPosition{
-	Position
+type UserPosition struct {
+	Position int "json/position_id"
 }
 
 func ReadAuthList(c *gin.Context) {
 	request := UserData{}
 
-	if err:= c.ShouldBindJSON(&request); err:= nil{
+	if err := c.ShouldBindJSON(&request); err != nil {
 		// エラーな場合、ステータス400と、エラー情報を返す
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	responce := UserPosition{}
+	responceDocument := UserPosition{}
 	db := infra.DBInitGorm()
 
-	db.Table("users").Select("position").Where("user_id = ?",request).First(&responce)
+	//役職IDを取得する
+	db.Table("users").Select("position").Where("user_id = ?", request).First(&responceDocument)
+	if db.Error != nil {
+		fmt.Print("ERROR!")
+	}
 
-	c.JSON(http.StatusOK, gin.H{"document": response.UserID})
+	//戻り値
+	c.JSON(http.StatusOK, gin.H{responceDocument})
 	return
 
 }
