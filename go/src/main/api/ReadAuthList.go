34795fba674d059d7331937f05954f6163a3f51b
Naga564 <yanagayakisoba@gmail.com>
2023-06-28 13:47:25 +0900

    新DB環境で動くように修正

--- go/src/main/api/ReadAuthList.go
+++ go/src/main/api/ReadAuthList.go
@@ -15,30 +15,30 @@ func ReadAuthList(c *gin.Context) {
 	response := []model.ReadAuthListResponse{}
 	take_class_id := model.TakeClassID{}
 
-	/*
+	//引数用・引数がなくなったら消す
 	request := model.ReadAuthListRequest{}
 	//POSTで受け取った値を格納する
 	if err := c.ShouldBindJSON(&request); err != nil {
 		// エラーな場合、ステータス400と、エラー情報を返す
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
-	}*/
+	}
 
 	//DB接続
 	db := infra.DBInitGorm()
 
 	// 本来ならミドルウェアの認証処理を挟み、取得したIDを格納する
-	id := 2
+	//id := 2
 
 	//サブクエリ(副問い合わせ)の作成
 	//所属クラス名を取得
-	db.Table("user").Select("post_id, class_id").Where("user_id = ?", id).First(&take_class_id)
+	db.Table("user").Select("post_id, class_id").Where("user_id = ?", request.UserID).First(&take_class_id)
 	// 実行予定のSQL
 	// SELECT post_id, class_id FROM user WHERE user_id = 2;
 
 	post := take_class_id.PostID - 1
 
-	if post == 1 {
+	if take_class_id.PostID == 1 {
 
 		//担任教員であるとき
 		//担任クラスのみ出力する
