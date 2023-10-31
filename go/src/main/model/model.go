cc89d796ebb00f7b9dcb3358ede2640cb4fbfa57
Naga564 <yanagayakisoba@gmail.com>
2023-10-31 13:02:53 +0900

    一応完成

--- go/src/main/model/model.go
+++ go/src/main/model/model.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"database/sql"
 	"time"
 )
 
@@ -72,23 +71,30 @@ type UserPosition struct {
 
 // CreateDocumentで使用する構造体
 type CreateDocumentRequest struct {
-	DocumentID        int            `json:"document_id"`         // ドキュメントID
-	StudentID         int            `json:"student_id"`          // 学籍番号
-	CompanyID         int            `json:"company_id"`          // 会社ID
-	ReasonID          int            `json:"reason_id"`           // 理由ID
-	RequestDate       time.Time      `json:"request_date"`        // 申請日
-	AbsenceStartDate  time.Time      `json:"absence_start_date"`  // 欠席開始日
-	AbsenceStartFlame int            `json:"absence_start_flame"` // 欠席開始時限
-	AbsenceEndDate    time.Time      `json:"absence_end_date"`    // 欠席終了日
-	AbsenceEndFlame   int            `json:"absence_end_flame"`   // 欠席終了時限
-	Location          string         `json:"location"`            // 場所
-	ReadFlag          bool           `json:"read_flag"`           // 既読フラグ
-	Status            int            `json:"status"`              // ステータス
-	StudentComment    sql.NullString `json:"student_comment"`     // 学生コメント
-	TeacherComment    sql.NullString `json:"teacher_comment"`     // 教員コメント
+	UserID         int       `json:"user_id"`         //ユーザID
+	RequestAt      time.Time `json:"request_at"`      // 申請日
+	StartTime      time.Time `json:"start_time"`      // 欠席開始日
+	EndTime        time.Time `json:"end_time"`        // 欠席終了日
+	Location       string    `json:"location"`        // 場所
+	StudentComment string    `json:"student_comment"` // 学生コメント
+	DivisionID     int       `json:"division_id"`     //区分ID
+}
+
+type CreateDocument struct {
+	UserID         int       `json:"user_id"`         //ユーザID
+	RequestAt      time.Time `json:"request_at"`      // 申請日
+	StartTime      time.Time `json:"start_time"`      // 欠席開始日
+	StartFlame     int       `json:"start_flame"`     //開始時限
+	EndTime        time.Time `json:"end_time"`        // 欠席終了日
+	EndFlame       int       `json:"end_flame"`       //開始時限
+	Location       string    `json:"location"`        // 場所
+	Status         int       `json:"status"`          //ステータス
+	ReadFlag       int       `json:"read_flag"`       //既読フラグ
+	StudentComment string    `json:"student_comment"` // 学生コメント
+	DivisionID     int       `json:"division_id"`     //区分ID
 }
 
 // DeleteDocumentで使用する構造体
-type DeleteDocumentRequest struct {
-	DocumentID int `json:"document_id"`
-}
+// type DeleteDocumentRequest struct {
+// 	DocumentID int `json:"document_id"`
+// }
