c9604930bc1e18f5492d399ff89ae1af793e2559
Naga564 <yanagayakisoba@gmail.com>
2023-10-31 10:25:05 +0900

    教員・学生ごとの処理を追加

--- go/src/main/api/NextDocument.go
+++ go/src/main/api/NextDocument.go
@@ -9,9 +9,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// type NextDocumentRequest struct {
+// 	DocumentID int `json:"document_id"` //書類ID
+// 	UserID     int `json:"user_id"`     //ユーザID
+// }
+
+// type NextDocumentResponse struct {
+// 	DocumentID     int       `json:"document_id"`     //書類ID
+// 	RequestAt      time.Time `json:"request_at"`      //申請日
+// 	StartTime      time.Time `json:"start_time"`      //公欠開始日時
+// 	StartFlame     int       `json:"start_flame"`     //公欠開始時限
+// 	EndTime        time.Time `json:"end_time"`        //公欠終了日時
+// 	EndFlame       int       `json:"end_flame"`       //公欠終了時限
+// 	Location       string    `json:"location"`        //場所
+// 	StudentComment string    `json:"student_comment"` //学生コメント
+// 	TeacherComment string    `json:"teacher_comment"` //教員コメント
+// 	UserNumber     int       `json:"user_number"`     //学籍番号
+// 	ClassAddr      string    `json:"class_addr"`      //クラス略称
+// 	DivisionName   string    `json:"division_name"`   //区分名
+// }
+
 func NextDocument(c *gin.Context) {
 	//必要な変数(引数・戻り値)の定義
-	request := []model.NextDocumentRequest{}
+	request := model.NextDocumentRequest{}
 	response := []model.NextDocumentResponse{}
 
 	var testUserID int
@@ -44,7 +64,7 @@ func NextDocument(c *gin.Context) {
 		//担任かどうかをチェック
 		if testUserID == request.UserID {
 
-			//認可すべき次の書類を取得・格納する
+			//認可すべき次の書類を取得・格納する(最新の書類)
 			db.Table("oa").
 				Select(
 					"oa.document_id",
@@ -62,13 +82,17 @@ func NextDocument(c *gin.Context) {
 				Joins("JOIN user on oa.user_id = user.user_id").
 				Joins("JOIN division AS dv ON oa.division_id = dv.division_id").
 				Joins("JOIN classification AS cs ON user.class_id = cs.class_id").
-				WHERE("user.user_id = ?", request.UserID).
-				Where("oa.status =", PostID-1)
-			Where()
-			Order("oa.request_at ASC").
+				Where("user.user_id = ?", request.UserID).
+				Where("oa.status =", PostID-1).
+				Order("oa.request_at ASC").
 				First(&response)
+
+			//エラーハンドリング
 			if db.Error != nil {
 				fmt.Print("SQL ERROR!")
+				c.JSON(http.StatusBadRequest, gin.H{
+					"message": "SQL ERROR",
+				})
 			}
 
 			//戻り値
@@ -77,8 +101,80 @@ func NextDocument(c *gin.Context) {
 			})
 
 		} else {
-			//リクエストが不正及び誤りがあるとき
+			//リクエストに不正及び誤りがあるとき(教員情報不一致)
 			c.JSON(http.StatusBadRequest, gin.H{"document": response})
 		}
+	} else if PostID >= 2 && PostID <= 5 {
+		//主任以上の教員処理
+		db.Table("oa").
+			Select(
+				"oa.document_id",
+				"oa.request_at",
+				"oa.start_time",
+				"oa.start_flame",
+				"oa.end_time",
+				"oa.end_flame",
+				"oa.location",
+				"oa.student_comment",
+				"oa.teacher_comment",
+				"user.user_number",
+				"cs.class_addr",
+				"dv.division_name").
+			Joins("JOIN user on oa.user_id = user.user_id").
+			Joins("JOIN division AS dv ON oa.division_id = dv.division_id").
+			Joins("JOIN classification AS cs ON user.class_id = cs.class_id").
+			Where("user.user_id = ?", request.UserID).
+			Where("oa.status =", PostID-1).
+			Order("oa.request_at ASC").
+			First(&response)
+		if db.Error != nil {
+			fmt.Print("SQL ERROR!")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "SQL ERROR",
+			})
+		}
+		//戻り値
+		c.JSON(http.StatusOK, gin.H{
+			"message": response,
+		})
+
+	} else if PostID == 0 {
+		//学生の処理
+		db.Table("oa").
+			Select(
+				"oa.document_id",
+				"oa.request_at",
+				"oa.start_time",
+				"oa.start_flame",
+				"oa.end_time",
+				"oa.end_flame",
+				"oa.location",
+				"oa.student_comment",
+				"oa.teacher_comment",
+				"user.user_number",
+				"cs.class_addr",
+				"dv.division_name").
+			Joins("JOIN user on oa.user_id = user.user_id").
+			Joins("JOIN division AS dv ON oa.division_id = dv.division_id").
+			Joins("JOIN classification AS cs ON user.class_id = cs.class_id").
+			Where("user.user_id = ?", request.UserID).
+			Where("oa.status =", PostID).
+			Order("oa.request_at ASC").
+			First(&response)
+		if db.Error != nil {
+			fmt.Print("SQL ERROR!")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "SQL ERROR",
+			})
+		}
+		//戻り値
+		c.JSON(http.StatusOK, gin.H{
+			"message": response,
+		})
+	} else {
+		//教員でも学生でもなかったとき(不正なデータ)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "POST ERROR",
+		})
 	}
 }
