f7f9b9010c7ce174af92985ba6f47ac611748b9d
suzukikaito244 <irookhihi@icloud.com>
2023-11-07 12:23:40 +0900

    管理者処理の追加

--- go/src/main/api/UpdateUser.go
+++ go/src/main/api/UpdateUser.go
@@ -2,12 +2,17 @@ package api
 
 import (
 	"net/http"
-
+	"fmt"
 	"main/infra"
 	"main/model"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Post struct {
+	PostID int
+}
+
 
 func UpdateUser(c *gin.Context) {
 
@@ -19,39 +24,48 @@ func UpdateUser(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	//DBに接続
+    db := infra.DBInitGorm()
+	//引数定義
+	post := Post{}
+	//post_idを取得(reqest.UserIDの部分に取得したいユーザのユーザIDを入れる)
+	db.Table("user").Select("post_id").Where("user_id = ?", request.UserID).First(&post)
 
-	//DB接続
-	db := infra.DBInitGorm()
-	//管理者の判定(未完成)
-	// post := post.PostID
+	PostID := post.PostID 
+
+	fmt.Println(PostID)
+
+	//管理者処理
+    if PostID == 0 {
+        
+		//更新処理
+		db.Table("user").
+		Where("user_id = ?", request.UserID).
+		Updates(model.UpdateUserRequest{
+			UserID: request.UserID,
+			UserName: request.UserName,
+			UserNumber: request.UserNumber,
+			PostID: request.PostID,
+			ClassID: request.ClassID,
+			MailAddress: request.MailAddress})
 	
+		//エラーハンドリング
+		if db.Error != nil {
+			errMsg := "UPDATE ERROR"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": errMsg})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"flag": "データの更新が成功しました"})
+	
+	//新たに管理者を増やすことができない
 	/*
-	if post == ? {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "権限がありません"})
-		return
-	}*/
-	
-	//更新処理
-	db.Table("user").
-	Where("user_id = ?", request.UserID).
-	Updates(model.UpdateUserRequest{
-		UserID: request.UserID,
-		UserName: request.UserName,
-		UserNumber: request.UserNumber,
-		PostID: request.PostID,
-		ClassID: request.ClassID,
-		MailAddress: request.MailAddress})
-	
-	//エラーハンドリング
-	if db.Error != nil {
-		errMsg := "UPDATE ERROR"
-		c.JSON(http.StatusInternalServerError, gin.H{"error": errMsg})
-		return
-	}
 
-	c.JSON(http.StatusOK, gin.H{"flag": "データの更新が成功しました"})
-	
-	//新たに管理者を増やすことができない(分からんそもそもUpdateUserで作るの？)
-	
+	*/
+	} else {
+		fmt.Println("権限がありません")
+		
+	}
+		  
 
 }
