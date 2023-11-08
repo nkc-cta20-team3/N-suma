f75a7b13caadd260ba218200b72662a0a437d03b
Naga564 <yanagayakisoba@gmail.com>
2023-11-08 10:36:34 +0900

    役職周りの引数による処理分け実装

--- go/src/main/api/ReadDocument.go
+++ go/src/main/api/ReadDocument.go
@@ -1,9 +1,7 @@
 package api
 
 import (
-	"log"
 	"net/http"
-	"fmt"
 
 	"main/infra"
 	"main/model"
@@ -60,73 +58,79 @@ func ReadDocument(c *gin.Context) {
 
 	PostID := post.PostID
 
-	fmt.Println(PostID)
+	// fmt.Println(PostID)
 	//管理者処理
 	if PostID == 0 {
-		fmt.Println("権限がありません")
-	}else if PostID == 1{
+		// fmt.Println("権限がありません")
+		c.JSON(http.StatusBadRequest, gin.H{"document": "権限がありません"})
+		return
+	} else if PostID == 1 {
 		//学生処理
 
-	// データベースからデータを取得する
-	db.Debug().Table("oa").
-		Select(
-			"oa.document_id",
-			"oa.request_at",
-			"oa.start_time",
-			"oa.start_flame",
-			"oa.end_time",
-			"oa.end_flame",
-			"oa.location",
-			"oa.student_comment",
-			"oa.teacher_comment",
-			"user.user_number",
-			"cs.class_name",
-			"user.user_name").
-		Joins("JOIN user ON oa.user_id = user.user_id").
-		Joins("JOIN classification AS cs ON user.class_id = cs.class_id").
-		Where("document_id = ?", request.DocumentID).
-		First(&response)
-	if db.Error != nil {
-		errMsg := "データベースからデータを取得できませんでした"
-		c.JSON(http.StatusInternalServerError, gin.H{"error": errMsg})
-		return
-	}
+		// データベースからデータを取得する
+		db.Debug().Table("oa").
+			Select(
+				"oa.document_id",
+				"oa.request_at",
+				"oa.start_time",
+				"oa.start_flame",
+				"oa.end_time",
+				"oa.end_flame",
+				"oa.location",
+				"oa.student_comment",
+				"oa.teacher_comment",
+				"user.user_number",
+				"cs.class_name",
+				"user.user_name").
+			Joins("JOIN user ON oa.user_id = user.user_id").
+			Joins("JOIN classification AS cs ON user.class_id = cs.class_id").
+			Where("document_id = ?", request.DocumentID).
+			First(&response)
+		if db.Error != nil {
+			errMsg := "データベースからデータを取得できませんでした"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": errMsg})
+			return
+		}
 
-	log.Println(response)
+		// log.Println(response)
 
-	c.JSON(http.StatusOK, gin.H{"document": response})
-	}else if PostID == 2{
+		c.JSON(http.StatusOK, gin.H{"document": response})
+	} else if PostID == 2 {
 		//教員処理
 
 		// データベースからデータを取得する
-	db.Debug().Table("oa").
-	Select(
-		"oa.document_id",
-		"oa.request_at",
-		"oa.start_time",
-		"oa.start_flame",
-		"oa.end_time",
-		"oa.end_flame",
-		"oa.location",
-		"oa.student_comment",
-		"oa.teacher_comment",
-		"user.user_number",
-		"cs.class_name",
-		"user.user_name").
-	Joins("JOIN user ON oa.user_id = user.user_id").
-	Joins("JOIN classification AS cs ON user.class_id = cs.class_id").
-	Where("document_id = ?", request.DocumentID).
-	First(&response)
-if db.Error != nil {
-	errMsg := "データベースからデータを取得できませんでした"
-	c.JSON(http.StatusInternalServerError, gin.H{"error": errMsg})
-	return
-}
+		db.Debug().Table("oa").
+			Select(
+				"oa.document_id",
+				"oa.request_at",
+				"oa.start_time",
+				"oa.start_flame",
+				"oa.end_time",
+				"oa.end_flame",
+				"oa.location",
+				"oa.student_comment",
+				"oa.teacher_comment",
+				"user.user_number",
+				"cs.class_name",
+				"user.user_name").
+			Joins("JOIN user ON oa.user_id = user.user_id").
+			Joins("JOIN classification AS cs ON user.class_id = cs.class_id").
+			Where("document_id = ?", request.DocumentID).
+			First(&response)
+		if db.Error != nil {
+			errMsg := "データベースからデータを取得できませんでした"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": errMsg})
+			return
+		}
 
-log.Println(response)
+		// log.Println(response)
 
-c.JSON(http.StatusOK, gin.H{"document": response})
-	}else {
-		fmt.Println("権限がありません")
+		c.JSON(http.StatusOK, gin.H{"document": response})
+		return
+	} else {
+		// fmt.Println("権限がありません")
+
+		c.JSON(http.StatusBadRequest, gin.H{"document": "権限がありません"})
+		return
 	}
 }
