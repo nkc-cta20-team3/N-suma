1a082c3421b6a3fcfef0cab8f49634b26f8c7026
Naga564 <yanagayakisoba@gmail.com>
2023-11-10 14:52:51 +0900

    classification,post,division表テストデータ完成

--- build/db/initdb.d/insert.sql
+++ build/db/initdb.d/insert.sql
@@ -2,37 +2,39 @@ USE cta20gr3;
 
 INSERT INTO `classification`(class_id,class_abbr,class_name)
 VALUES
-(117,'CTA20','情報総合学科4年A組'),
-(118,'CTB20','情報総合学科4年B組'),
+(132,'AI22','AIシステム科2年'),
+(123,'CSX21','情報システム科3年'),
 (115,'CTA21','情報総合学科3年A組'),
-(116,'CTB21','情報総合学科3年B組');
+(117,'CTA20','情報総合学科4年A組'),
+(118,'CTB20','情報総合学科4年B組');
+
 
 INSERT INTO `post`(post_id,post_name)
 VALUES
 (0,'管理者'),
 (1,'学生'),
-(2,'担任'),
-(3,'主任'),
-(4,'学科長'),
-(5,'学部長'),
-(6,'所属長');
+(2,'教員');
 
 INSERT INTO `division`(division_id,division_name,division_detail,division_remark)
 VALUES
-(100,'国家試験','FE','基本情報'),
-(101,'国家試験','AP','応用情報'),
-(102,'国家試験','SG','情報セキュリティマネジメント');
+(100,'国家試験','FE','基本情報処理技術者試験'),
+(101,'国家試験','AP','応用情報処理技術者試験'),
+(102,'国家試験','SC','情報処理安全確保支援士試験'),
+(200,'就職活動','面接試験(1次)',''),
+(201,'就職活動','面接試験(最終)',''),
+(300,'忌引','葬儀','休暇は3日まで'),
+(400,'その他','その他','担任と要相談');
 
 INSERT INTO `user`(user_uuid,user_name,user_number,post_id,class_id,mail_address,user_flag)
 VALUES
-('1waffa4r','田中太郎',20206000,1,117,'aaa.gmail.com',true),
-('ff3ru6dg','高橋啓太',null,2,117,'bbb.gmail.com',true),
-('ohigw923','藤井雄太',null,3,null,'ccc.gmail.com',true),
-('foqihnfl','斎藤勇',null,4,null,'ddd.gmail.com',true),
-('o32tgbkl','小澤直樹',null,5,null,'eee.gmail.com',true),
-('4hpivesw','大橋健一',null,6,null,'fff.gmail.com',true),
-('9vacugh2','櫻井健一',20206100,1,116,'ggg.gmail.com',true),
-('36gew305','鈴木秀昭',20206200,1,118,'hhh.gmail.com',true);
+('1waffa4r','田中太郎',20226000,1,132,'aaa.gmail.com',true),
+('ff3ru6dg','田中次郎',20226002,1,132,'bbb.gmail.com',true),
+('ohigw923','佐藤一子',20216003,1,115,'ccc.gmail.com',true),
+('foqihnfl','佐藤次子',20216004,1,115,'ddd.gmail.com',true),
+('o32tgbkl','森田林',20206005,1,117,'eee.gmail.com',true),
+('4hpivesw','熱田神男',20206006,1,117,'fff.gmail.com',true),
+('9vacugh2','秋田冬美',20206007,1,118,'ggg.gmail.com',true),
+('36gew305','上田　下',20206008,1,118,'hhh.gmail.com',true),
 
 INSERT INTO `oa`(request_at,start_time,start_flame,end_time,end_flame,location,status,read_flag,student_comment,teacher_comment,user_id,division_id)
 VALUES
