85e88530c02e557caacdfa8ded4a475bf3ebf2b9
Naga564 <yanagayakisoba@gmail.com>
2023-11-10 15:00:29 +0900

    user表データ完成

--- build/db/initdb.d/insert.sql
+++ build/db/initdb.d/insert.sql
@@ -35,6 +35,15 @@ VALUES
 ('4hpivesw','熱田神男',20206006,1,117,'fff.gmail.com',true),
 ('9vacugh2','秋田冬美',20206007,1,118,'ggg.gmail.com',true),
 ('36gew305','上田　下',20206008,1,118,'hhh.gmail.com',true),
+('y5r2a6p8',' kebin fake ',20206009,1,118,'iii.gmail.com',true),
+('a5p2r9q0','虚無田無心',null,2,null,'jjj.gmail.com',true),
+('z3c7d8x1','加藤聡美',null,2,123,'kkk.gmail.com',true),
+('k6o4s9f2','遠山千佳',null,2,115,'lll.gmail.com',true),
+('e1v0m8y7','啓発登',null,2,118,'mmm.gmail.com',true),
+('h2t5l6n3','和井田昇',null,2,118,'nnn.gmail.com',true),
+('d6t2w9v8','無所俗助',null,2,null,'ooo.gmail.com',true),
+('h4o1z7f3','江藤　近	',null,0,null,'ppp.gmail.com',true),
+('q5p0r9a2','辞田博志',null,2,null,'qqq.gmail.com',false);
 
 INSERT INTO `oa`(request_at,start_time,start_flame,end_time,end_flame,location,status,read_flag,student_comment,teacher_comment,user_id,division_id)
 VALUES
