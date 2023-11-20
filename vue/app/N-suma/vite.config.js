908d7ef45c0506e6a78929e60cea4fc3d61908b1
yukke23 <ec98721zzz@gmail.com>
2023-11-20 14:38:41 +0900

    fix vite config,import code.

--- vue/app/N-suma/vite.config.js
+++ vue/app/N-suma/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
-import { vue } from "@vitejs/plugin-vue";
-import { vuetify } from "vite-plugin-vuetify";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
