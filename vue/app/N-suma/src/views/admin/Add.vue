e673aabe199451d08624787aa3949aefb21cb210
TYT77 <ec98721zzz@gmail.com>
2023-11-29 00:52:51 +0900

    update page admin add

--- vue/app/N-suma/src/views/admin/Add.vue
+++ vue/app/N-suma/src/views/admin/Add.vue
@@ -3,7 +3,7 @@
     <v-row justify="center">
       <v-col cols="12" sm="10" md="8" lg="6">
         <!-- 入力フォーム -->
-        <v-form @submit.prevent="onSubmit">
+        <v-form ref="mainForm">
           <!-- UUID選択 -->
           <v-select
             v-model="state.uuid"
@@ -20,7 +20,7 @@
             persistent-hint
             placeholder="aaaa1234@example.com"
             persistent-placeholder
-            :rules="[(v) => !!v || '必須']"
+            :rules="requiredRule"
             @update:modelValue="emailSelected"
           ></v-select>
 
@@ -31,7 +31,7 @@
             persistent-hint
             placeholder="田中太郎"
             persistent-placeholder
-            :rules="[(v) => !!v || '必須']"
+            :rules="requiredRule"
           ></v-text-field>
 
           <!-- 学籍番号入力 -->
@@ -45,6 +45,7 @@
           ></v-text-field>
 
           <!-- クラス略称入力 -->
+          <!--
           <v-text-field
             v-model="state.class"
             label="クラス略称"
@@ -53,6 +54,19 @@
             persistent-placeholder
             :rules="classRules"
           ></v-text-field>
+          -->
+
+          <!-- TODO&MEMO:クラス略称の一覧が入手できた場合、ハードコーディングしてしまった方がよさそう -->
+          <!-- クラス略称選択 -->
+          <v-select
+            v-model="state.class"
+            :items="classes"
+            label="クラス略称"
+            persistent-hint
+            placeholder="CTA20"
+            persistent-placeholder
+            :rules="requiredRule"
+          ></v-select>
 
           <!-- 役職選択 -->
           <v-select
@@ -62,16 +76,14 @@
             persistent-hint
             placeholder="学生"
             persistent-placeholder
-            :rules="[(v) => !!v || '必須']"
+            :rules="requiredRule"
           ></v-select>
 
           <!-- 登録ボタン -->
           <template class="d-flex flex-row justify-end text-black">
             <v-dialog transition="dialog-top-transition" width="auto">
               <template v-slot:activator="{ props }">
-                <v-btn height="40" width="150" v-bind="props" type="submit"
-                  >登録</v-btn
-                >
+                <v-btn height="40" width="150" v-bind="props">登録</v-btn>
               </template>
               <template v-slot:default="{ isActive }">
                 <v-card>
@@ -88,7 +100,12 @@
                     <v-btn
                       height="40"
                       width="150"
-                      @click="isActive.value = false"
+                      @click="
+                        () => {
+                          isActive.value = false;
+                          onSubmit();
+                        }
+                      "
                       >登録</v-btn
                     >
                   </v-card-actions>
@@ -105,6 +122,8 @@
 <script setup>
 import { onMounted, ref } from "vue";
 
+const mainForm = ref(null);
+
 const state = ref({
   uuid: "",
   email: "",
@@ -117,29 +136,39 @@ const state = ref({
 // TODO: 役職が未定義のユーザー一覧を取得する(uuidとemailの組)
 const uuids = ["1", "2", "3", "4"];
 const emails = ["11", "22", "33", "44"];
+
+const classes = ["CTA20", "CTB20", "CTC20", "CTD20", "CTE20", "CTF20"];
 const roles = ["担任", "学生"];
 
+const requiredRule = [(v) => !!v || "必須"];
+
 const numberRules = [
-  (v) => !!v || "必須",
   (v) => v.length == 8 || "学籍番号は8桁です",
   (v) => /^\d+$/.test(v) || "学籍番号は半角数字です",
+  (v) => !!v || "必須",
 ];
 
 // TODO:クラス略称は5桁なのか確認する
 // それか、クラス略称をDBから取得する？
 const classRules = [
-  (v) => !!v || "必須",
   (v) => v.length == 5 || "クラス略称は5桁です",
   (v) => /^[A-Z0-9]+$/.test(v) || "クラス略称は大文字半角英字と半角数字です",
+  (v) => !!v || "必須",
 ];
 
 function emailSelected(e) {
   state.value.uuid = uuids[emails.indexOf(e)];
 }
 
-function onSubmit() {
-  // ユーザーを登録する処理を記述する
-  console.log("ユーザーを登録しました");
+async function onSubmit() {
+  const validResult = await mainForm.value.validate();
+
+  if (validResult.valid) {
+    // ユーザーを登録する処理を記述する
+    console.log("ユーザーを登録しました");
+  } else {
+    console.log("ユーザーを登録できませんでした");
+  }
 }
 
 onMounted(() => {
