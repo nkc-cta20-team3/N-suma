5dc746659505084c25e03edbf2c78102833af07f
yukke23 <ec98721zzz@gmail.com>
2023-11-28 14:58:08 +0900

    update main.js

--- vue/app/N-suma/src/main.js
+++ vue/app/N-suma/src/main.js
@@ -7,23 +7,3 @@ import App from "@/App.vue";
 const app = createApp(App);
 registerPlugins(app);
 app.mount("#app");
-
-
-onAuthStateChanged(auth, (u) => {
-  // 取得したユーザー情報を格納する
-  // ユーザーがログインしていない場合はnullを返す
-  user.value = u;
-
-  // ユーザー情報が取得できた場合に、役職を取得するAPIを呼び出す
-  //ここで取得した役職をstoreに格納し、ログイン状態を管理する
-  if (u) {
-    // TODO: getRole()を実装する
-    // role.value = getRole(u)
-    role.value = "admin";
-
-    isLogin.value = true;
-  } else {
-    role.value = null;
-    isLogin.value = false;
-  }
-});
