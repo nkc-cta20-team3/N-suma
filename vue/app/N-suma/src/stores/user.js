c5a5eae6c37266af2ce507e73374c9fd319b019f
yukke23 <ec98721zzz@gmail.com>
2023-11-28 14:57:32 +0900

    update user store

--- vue/app/N-suma/src/stores/user.js
+++ vue/app/N-suma/src/stores/user.js
@@ -5,10 +5,10 @@
  * used pinia
  * documentation: https://pinia.vuejs.org/
  */
-import { ref } from "vue";
 import { defineStore } from "pinia";
 import { auth, provider } from "@/plugins/firebase";
 import { onAuthStateChanged, signInWithRedirect, signOut } from "firebase/auth";
+import router from "@/router";
 
 export const useStore = defineStore("user", {
   state: () => {
@@ -38,6 +38,38 @@ export const useStore = defineStore("user", {
     // ログアウトする
     async logout() {
       await signOut(auth);
+      this.resetData();
+      router.push("/");
+    },
+    // ログイン状態を確認し、ログインしていればユーザー情報を取得する
+    getLoginState() {
+      return new Promise((resolve, reject) => {
+        const unsuscribe = onAuthStateChanged(
+          auth,
+          (u) => {
+            if (u) {
+              this.user = u;
+              // TODO: getRole()を実装する
+              //this.role = getRole(u.uid);
+              this.role = "admin";
+              this.isLogin = true;
+              //console.log("user is login");
+              resolve(true);
+            } else {
+              this.user = null;
+              //console.log("user is none data");
+            }
+            resolve(false);
+          },
+          (e) => reject(e)
+        );
+        unsuscribe();
+      });
+    },
+    resetData() {
+      this.user = null;
+      this.role = null;
+      this.isLogin = false;
     },
   },
 });
