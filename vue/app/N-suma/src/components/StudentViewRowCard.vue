50039b949e22a521c53a361f4b5215d8e539270d
TYT77 <ec98721zzz@gmail.com>
2023-11-30 00:38:47 +0900

    create component rowcard

---
+++ vue/app/N-suma/src/components/StudentViewRowCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row class="d-flex flex-row text-black pt-2">
+    <v-card width="35%">
+      <v-card-title>{{ props.title }}</v-card-title>
+    </v-card>
+    <v-card width="65%" @click.stop="dialog = true">
+      <v-card-title>{{ props.text }}</v-card-title>
+    </v-card>
+  </v-row>
+  <v-dialog width="70%" v-model="dialog" scrollable>
+    <v-card @click.stop="dialog = true">
+      <v-card-text>{{ props.text }}</v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+import { defineProps, ref } from "vue";
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+const dialog = ref(false);
+</script>
