6b06512a9a8531b6aa8c2d4e175e60f08e7d7dbe
yukke23 <ec98721zzz@gmail.com>
2023-11-30 13:09:04 +0900

    update pages student reform

--- vue/app/N-suma/src/views/student/ReForm.vue
+++ vue/app/N-suma/src/views/student/ReForm.vue
@@ -4,124 +4,79 @@
       <v-col cols="12" sm="10" md="8" lg="6">
         <!-- 入力フォーム -->
         <v-form ref="mainForm">
-          <!-- UUID選択 -->
+          <!-- 公欠区分選択 -->
           <v-select
-            v-model="state.uuid"
-            label="uuid"
+            v-model="state.division"
+            :items="divisions"
+            label="公欠区分"
             persistent-hint
-            :disabled="true"
-          ></v-select>
-
-          <!-- Eメール選択 -->
-          <v-select
-            v-model="state.email"
-            label="Eメール"
-            persistent-hint
-            :disabled="true"
-          ></v-select>
-
-          <!-- 氏名入力 -->
-          <v-text-field
-            v-model="state.name"
-            label="氏名"
-            persistent-hint
-            placeholder="田中太郎"
+            placeholder="国家試験 / FE"
             persistent-placeholder
             :rules="requiredRules"
+          ></v-select>
+
+          <!-- 公欠開始/終了時刻選択 -->
+          <v-card width="100%"> 公欠開始/終了時刻 </v-card>
+          <template class="d-flex flex-row text-black">
+            <VueDatePicker
+              v-model="date"
+              :preview-format="format"
+              :format="format"
+              :enable-time-picker="true"
+              week-start="0"
+              locale="jp"
+              placeholder="日付を選択"
+              range
+            ></VueDatePicker>
+          </template>
+
+          <!-- 実施場所入力 -->
+          <v-text-field
+            v-model="state.location"
+            label="実施場所"
+            persistent-hint
+            placeholder="愛知県名古屋市熱田区神宮 / 名古屋工学院専門学校"
+            persistent-placeholder
+            :rules="requiredRules"
+            class="mt-4"
           ></v-text-field>
 
-          <!-- 学籍番号入力 -->
+          <!-- 学生コメント入力 -->
           <v-text-field
-            v-model="state.number"
-            label="学籍番号"
+            v-model="state.studentComment"
+            label="学生コメント"
             persistent-hint
-            placeholder="20201001"
+            placeholder="確認よろしくお願いいたします。"
             persistent-placeholder
-            :rules="numberRules"
+            :rules="requiredRules"
+            :counter="200"
           ></v-text-field>
 
-          <!-- クラス略称選択 -->
-          <v-select
-            v-model="state.class"
-            :items="clases"
-            label="クラス略称"
+          <!-- 教員コメント表示 -->
+          <v-text-field
+            v-model="state.teacherComment"
+            label="教員コメント"
             persistent-hint
-            placeholder="CTA20"
-            persistent-placeholder
-            :rules="requiredRules"
-          ></v-select>
+            :disabled="true"
+          ></v-text-field>
 
-          <!-- 役職選択 -->
-          <v-select
-            v-model="state.role"
-            :items="roles"
-            label="役職"
-            persistent-hint
-            placeholder="学生"
-            persistent-placeholder
-            :rules="requiredRules"
-          ></v-select>
-
-          <!-- 各種ボタン -->
+          <!-- 再提出ボタン -->
           <template class="d-flex flex-row justify-end text-black">
-            <!-- 削除ボタン -->
             <v-dialog transition="dialog-top-transition" width="auto">
               <template v-slot:activator="{ props }">
                 <v-btn
                   height="40"
                   width="150"
                   v-bind="props"
-                  type="submit"
-                  color="warning"
-                  class="mr-2"
-                  >削除</v-btn
-                >
-              </template>
-              <template v-slot:default="{ isActive }">
-                <v-card>
-                  <v-card-text>
-                    <div>ユーザーを削除しますか?</div>
-                  </v-card-text>
-                  <v-card-actions class="justify-center">
-                    <v-btn
-                      height="40"
-                      width="150"
-                      @click="isActive.value = false"
-                      color="success"
-                      >キャンセル</v-btn
-                    >
-                    <v-btn
-                      height="40"
-                      width="150"
-                      @click="
-                        () => {
-                          isActive.value = false;
-                          onDelete();
-                        }
-                      "
-                      color="warning"
-                      >削除</v-btn
-                    >
-                  </v-card-actions>
-                </v-card>
-              </template>
-            </v-dialog>
-            <!-- 更新ボタン -->
-            <v-dialog transition="dialog-top-transition" width="auto">
-              <template v-slot:activator="{ props }">
-                <v-btn
-                  height="40"
-                  width="150"
-                  v-bind="props"
-                  type="submit"
                   color="success"
-                  >更新</v-btn
+                  class="mr-2"
+                  >再提出</v-btn
                 >
               </template>
               <template v-slot:default="{ isActive }">
                 <v-card>
                   <v-card-text>
-                    <div>ユーザーを更新しますか?</div>
+                    <div>書類を再提出しますか?</div>
                   </v-card-text>
                   <v-card-actions class="justify-center">
                     <v-btn
@@ -137,11 +92,11 @@
                       @click="
                         () => {
                           isActive.value = false;
-                          onUpdate();
+                          onSubmit();
                         }
                       "
                       color="success"
-                      >登録</v-btn
+                      >提出</v-btn
                     >
                   </v-card-actions>
                 </v-card>
@@ -156,67 +111,72 @@
 
 <script setup>
 import { onMounted, ref } from "vue";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import { onBeforeRouteUpdate } from "vue-router";
 import router from "@/router";
+import { requiredRules, divisions } from "@/utils";
 
-const roles = ["学生", "担任"];
-const clases = [
-  "CTA20",
-  "CTB20",
-  "CTA21",
-  "CTB21",
-  "CTA22",
-  "CTB22",
-  "CTA23",
-  "CTB23",
-];
-
-const requiredRules = [(v) => !!v || "必須"];
-
-const numberRules = [
-  (v) => !!v || "必須",
-  (v) => v.length == 8 || "学籍番号は8桁です",
-  (v) => /^\d+$/.test(v) || "学籍番号は半角数字です",
-];
+const format = "yyyy-MM-dd HH:mm";
 
 const mainForm = ref(null);
 const state = ref({
-  uuid: "",
-  email: "",
-  name: "",
-  number: "",
-  class: "",
-  role: "",
+  id: "",
+  division: "",
+  startDate: "",
+  endDate: "",
+  location: "",
+  studentComment: "",
+  teacherComment: "",
 });
+const date = ref(null);
 
-async function onUpdate() {
+async function onSubmit() {
+  console.log(state.value);
+
+  // 日付の入力チェック
+  if (!date.value || !date.value[0] || !date.value[1]) {
+    console.log("日付入力不足");
+    // TODO: エラーを画面に表示する処理を記述する？
+    return;
+  }
+  // 日付以外の入力チェック
   const validResult = await mainForm.value.validate();
   if (!validResult.valid) {
     console.log("入力エラー");
     return;
   }
+  state.value.startDate = date.value[0];
+  state.value.endDate = date.value[1];
 
-  // TODO:ユーザーを更新する処理を記述する
-  console.log("ユーザーを更新しました");
-}
+  // TODO:書類を提出する処理を記述する
+  console.log("提出しました");
 
-async function onDelete() {
-  const validResult = await mainForm.value.validate();
-  if (!validResult.valid) {
-    console.log("入力エラー");
-    return;
-  }
-
-  // TODO:ユーザーを削除する処理を記述する
-  console.log("ユーザーを削除しました");
+  // 画面遷移
+  router.push("/student/list");
 }
 
 onMounted(() => {
   console.log("mounted");
-  // TODO: routerのpropsからユーザー情報を取得する(uuidとemailの組)
-
-  state.value.uuid = router.currentRoute.value.params.id;
-  state.value.email = "11@example.com";
-
-  // TODO: ユーザーの詳細な情報を取得し設定する
+  // TODO: routerのpropsから書類のidを取得し、stateに設定する
+  state.value.id = router.currentRoute.value.params.id;
+  init();
 });
+
+// TODO: 画面遷移時に呼ばれる処理を記述する
+onBeforeRouteUpdate((to, from, next) => {
+  console.log("beforeRouteUpdate");
+  // TODO: 画面更新時に呼び出されるため、to.params.idをもとにstateを初期化する
+  state.value.id = to.params.id;
+  init();
+  next();
+});
+
+function init() {
+  // TODO: 書類のidをもとに、APIを叩いて詳細な情報を取得し設定する
+  state.value.division = "国家試験";
+  state.value.location = "愛知県名古屋市熱田区神宮";
+  state.value.studentComment = "確認よろしくお願いいたします。";
+  state.value.teacherComment = "確認しました。";
+}
 </script>
