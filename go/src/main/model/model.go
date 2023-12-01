15ba3fbd699783d7dae4c2146d88b336093b44d0
Naga564 <yanagayakisoba@gmail.com>
2023-12-01 15:05:28 +0900

    createdocument

--- go/src/main/model/model.go
+++ go/src/main/model/model.go
@@ -109,28 +109,42 @@ type UserPosition struct {
 
 // CreateDocumentで使用する構造体
 type CreateDocumentRequest struct {
-	UserID         int       `json:"user_id"`         //ユーザID
-	RequestAt      time.Time `json:"request_at"`      // 申請日
-	StartTime      time.Time `json:"start_time"`      // 欠席開始日
-	EndTime        time.Time `json:"end_time"`        // 欠席終了日
-	Location       string    `json:"location"`        // 場所
-	StudentComment string    `json:"student_comment"` // 学生コメント
-	DivisionID     int       `json:"division_id"`     //区分ID
+	UserID         int    `json:"user_id"`         //ユーザID
+	RequestAt      string `json:"request_at"`      // 申請日
+	StartTime      string `json:"start_time"`      // 欠席開始日
+	EndTime        string `json:"end_time"`        // 欠席終了日
+	Location       string `json:"location"`        // 場所
+	StudentComment string `json:"student_comment"` // 学生コメント
+	DivisionID     int    `json:"division_id"`     //区分ID
 }
 
 type CreateDocument struct {
-	UserID         int       // ユーザID
-	RequestAt      time.Time // 申請日
-	StartTime      time.Time // 欠席開始日
-	StartFlame     int       // 開始時限
-	EndTime        time.Time // 欠席終了日
-	EndFlame       int       // 終了時限
-	Location       string    // 場所
-	Status         int       // 認可状態
-	StudentComment string    // 学生コメント
-	ReadFlag       bool      // 既読フラグ
-	DivisionID     int       // 区分ID
+	UserID         int    // ユーザID
+	RequestAt      string // 申請日
+	StartTime      string // 欠席開始日
+	StartFlame     int    // 開始時限
+	EndTime        string // 欠席終了日
+	EndFlame       int    // 終了時限
+	Location       string // 場所
+	Status         int    // 認可状態
+	StudentComment string // 学生コメント
+	ReadFlag       bool   // 既読フラグ
+	DivisionID     int    // 区分ID
 }
+
+// type CreateDocument struct {
+// 	UserID         int       // ユーザID
+// 	RequestAt      time.Time // 申請日
+// 	StartTime      time.Time // 欠席開始日
+// 	StartFlame     int       // 開始時限
+// 	EndTime        time.Time // 欠席終了日
+// 	EndFlame       int       // 終了時限
+// 	Location       string    // 場所
+// 	Status         int       // 認可状態
+// 	StudentComment string    // 学生コメント
+// 	ReadFlag       bool      // 既読フラグ
+// 	DivisionID     int       // 区分ID
+// }
 
 // ResubmitDocumentで使用する構造体
 type ResubmitDocumentRequest struct {
