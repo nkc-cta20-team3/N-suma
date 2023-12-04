41d84fa72ecbfb438c94f2eb4800cf795b6f117d
masakiwakida <bluekakarotto.7s@ezweb.ne.jp>
2023-12-04 14:45:29 +0900

    おｋ

--- vue/app/N-suma/src/views/teacher/List.vue
+++ vue/app/N-suma/src/views/teacher/List.vue
@@ -1,29 +1,5 @@
 <template>
   <v-container>
-    <!-- 検索バー -->
-    <v-row justify="center">
-      <v-col cols="12" sm="10" md="8" lg="6" class="pt-10">
-        <template class="d-flex flex-row justify-end text-black">
-          <!-- 学籍番号入力 -->
-          <v-text-field
-            v-model="number"
-            label="学籍番号"
-            persistent-hint
-            placeholder="20201001"
-            persistent-placeholder
-            :counter="8"
-            class="mr-2"
-          ></v-text-field>
-
-          <!-- 検索ボタン -->
-          <v-btn @click="onSearch" icon>
-            <!-- icon url : https://pictogrammers.com/library/mdi/icon/magnify/ -->
-            <v-icon alt="search icon" :icon="mdiMagnify"></v-icon>
-          </v-btn>
-        </template>
-      </v-col>
-    </v-row>
-
     <!-- 書類一覧 -->
     <v-row justify="center">
       <v-col cols="12" sm="10" md="8" lg="6">
