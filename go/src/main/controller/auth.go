a13ad2c77a853a73d95b41d0a8c838849215e8fb
yukke23 <ec98721zzz@gmail.com>
2023-12-06 13:53:22 +0900

    create Go authMiddleware

---
+++ go/src/main/controller/auth.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+	
+)
+
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		// Firebase SDK のセットアップ
+		opt := option.WithCredentialsFile(os.Getenv("GOOGLE_CREDENTIALS_JSON"))
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			log.Printf("err: %v\n", err)
+			os.Exit(1)
+		}
+		auth, err := app.Auth(context.Background())
+		if err != nil {
+			log.Printf("err: %v\n", err)
+			os.Exit(1)
+		}
+
+		// AuthorizationヘッダからIDトークン(JWT)を取得
+		authHandler := c.Request.Header.Get("Authorization")
+		idToken := strings.Replace(authHandler, "Bearer ", "", 1)
+
+		// IDトークンを検証
+		token, err := auth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			// 検証エラー
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"Error": err.Error(),
+			})
+			return
+		}
+		// ロギング
+		log.Printf("Vertifed ID token: %v\n", token)
+		c.Next()
+	}
+}
