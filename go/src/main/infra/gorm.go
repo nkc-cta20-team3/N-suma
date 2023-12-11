599972edf4fdf103b76ba6a06e0d79dc85309987
yukke23 <ec98721zzz@gmail.com>
2023-12-11 14:19:07 +0900

    update go infra enable logger

--- go/src/main/infra/gorm.go
+++ go/src/main/infra/gorm.go
@@ -6,11 +6,13 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func DBInitGorm() *gorm.DB {
 	// データベース接続
 	db, err := gorm.Open(mysql.Open(os.Getenv("DB_HOST")))
+	db.Logger = db.Logger.LogMode(logger.Info) // ログ出力設定
 	// データベース接続エラー
 	if err != nil {
 		log.Println(err)
