24fe1dc603e5025daa7e6e2cc8397a000f55b07d
yukke23 <ec98721zzz@gmail.com>
2023-12-11 14:23:33 +0900

    fix go model admin

--- go/src/main/model/admin.go
+++ go/src/main/model/admin.go
@@ -2,8 +2,8 @@ package model
 
 // ReadPrepareInformationResponse は、役職が未登録ユーザーのid,uuid,emailをリストで一覧取得する際に使用する構造体
 type ReadPrepareInformationResponse struct {
-	UserID   int    `json:"user_id"`
-	UserUUID  string 	`json:"user_uuid"`
+	UserID  	int		`json:"user_id"`
+	UserUUID  	string 	`json:"user_uuid"`
 	MailAddress string 	`json:"mail_address"`
 }	
 
@@ -55,37 +55,36 @@ type ReadUserResponse struct {
 	MailAddress string 	`json:"mail_address"`
 	UserName   	string 	`json:"user_name"`
 	UserNumber 	int    	`json:"user_number"`
-	ClassID  	string 	`json:"class_id"`
+	ClassID  	int 	`json:"class_id"`
 	PostID    	int    	`json:"post_id"`
 	UserFlag  	bool   	`json:"user_flag"`
 }
 
 // UpdateUser は、ユーザー情報を更新する際に、クライアントからのデータを受け取るのに使用する構造体
 type UpdateUserRequest struct {
-	UserID       int    `json:"user_id"`
-	UserName     string `json:"user_name"`
-	UserNumber   int    `json:"user_number"`
-	ClassID      int    `json:"class_id"`
-	PostID       int    `json:"post_id"`
-	UserFlag  	bool   	`json:"user_flag"`
+	UserID       	int		`json:"user_id"`
+	UserName     	string 	`json:"user_name"`
+	UserNumber   	int    	`json:"user_number"`
+	ClassID      	int    	`json:"class_id"`
+	PostID      	int    	`json:"post_id"`
+	UserFlag  		bool   	`json:"user_flag"`
 }
-
 
 // UpdateUserStruct は、ユーザー情報を更新する際に、DBのデータを更新するときに使用する構造体
 type UpdateUserStruct struct {
-	UserName     string `json:"user_name"`
-	UserNumber   int    `json:"user_number"`
-	PostID       int    `json:"post_id"`
-	ClassID      int    `json:"class_id"`
-	UserFlag  	bool   	`json:"user_flag"`
+	UserName    string 	`json:"user_name"`
+	UserNumber  *int    	`json:"user_number"`
+	PostID      *int    	`json:"post_id"`
+	ClassID     *int    	`json:"class_id"`
+	UserFlag  	*bool   	`json:"user_flag"`
 }
 
 // DeleteUserRequest は、ユーザー情報を削除する際に、クライアントからのデータを受け取るのに使用する構造体
 type DeleteUserRequest struct {
-	TargetUserID int `json:"user_id"`
+	TargetUserID	int	`json:"user_id"`
 }
 
 // DeleteUserStruct は、ユーザー情報を削除する際に、DBのデータを更新するときに使用する構造体
 type DeleteUserStruct struct {
-	UserFlag bool `json:"user_flag"`
+	UserFlag	*bool	`json:"user_flag"`
 }
