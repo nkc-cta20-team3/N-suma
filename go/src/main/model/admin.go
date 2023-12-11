d85d546961a71397974471009971cdc7625c3c1b
yukke23 <ec98721zzz@gmail.com>
2023-12-11 12:55:03 +0900

    fix go model admin

--- go/src/main/model/admin.go
+++ go/src/main/model/admin.go
@@ -9,11 +9,19 @@ type ReadPrepareInformationResponse struct {
 
 // CreateUserRequest は、ユーザー作成時に必要な情報を受け取るために使用する構造体
 type CreateUserRequest struct {
-	UserID      int    `json:"user_id"`
-	UserName    string `json:"user_name"`
-	UserNumber  *string `json:"user_number"`
-	PostID      int    `json:"post_id"`
-	ClassID     int    `json:"class_id"`
+	UserID      int    	`json:"user_id"`
+	UserName    string 	`json:"user_name"`
+	UserNumber  string	`json:"user_number"`
+	PostID      int    	`json:"post_id"`
+	ClassID     int    	`json:"class_id"`
+}
+
+// CreateUserStruct は、ユーザー情報を更新する際に、DBのデータを更新するときに使用する構造体
+type CreateUserStruct struct {
+	UserName    string 	`json:"user_name"`
+	UserNumber  *int	`json:"user_number"`
+	PostID      int    	`json:"post_id"`
+	ClassID     int    	`json:"class_id"`
 }
 
 // ReadUserListResponse は、ユーザー一覧を取得する際に使用する構造体
