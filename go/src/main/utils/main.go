9b0c2bffed2cb9aaba91428deedb3a2184d3f1ed
TYT77 <ec98721zzz@gmail.com>
2023-12-12 01:51:12 +0900

    update go utils add func

--- go/src/main/utils/main.go
+++ go/src/main/utils/main.go
@@ -44,3 +44,20 @@ func StringToTime2(stringTime string) time.Time {
 	}
 	return time
 }
+
+// 「2023-12-11T15:39:00.000Z」を「2023-12-11 15:39」に変換
+func TimeToString2(time *time.Time) string {
+	// 時刻型を文字列に変換
+	stringTime := time.Format("2006-01-02 15:04")
+	return stringTime
+}
+
+// 「2023-12-11T00:00:00Z」を「2023-12-11」に変換
+func StringToTime3(stringTime string) time.Time {
+	// 文字列を時刻型に変換
+	time, err := time.Parse("2006-01-02T00:00:00Z", stringTime)
+	if err != nil {
+		log.Println(err)
+	}
+	return time
+}
