53072b4d6b2ad8d5b04d903dc1cc7a603f200746
TYT77 <ec98721zzz@gmail.com>
2023-12-12 01:02:15 +0900

    update go utils add func

--- go/src/main/utils/main.go
+++ go/src/main/utils/main.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,4 +15,32 @@ func LogOutput(message string) {
 // アクセス元のIPアドレスを出力する
 func LogClientIP(c *gin.Context) {
 	log.Printf("ClientIP: %s\n", c.ClientIP())
+}
+
+// 文字列を時刻型に変換する関数
+func StringToTime(stringTime string) time.Time {
+	// 文字列を時刻型に変換
+	time, err := time.Parse("2006-01-02 15:04", stringTime)
+	// TODO: エラーハンドリング
+	if err != nil {
+		log.Println(err)
+	}
+	return time
+}
+
+// 時刻型を文字列に変換する関数
+func TimeToString(time *time.Time) string {
+	// 時刻型を文字列に変換
+	stringTime := time.Format("2006-01-02 15:04")
+	return stringTime
+}
+
+// 「2023-12-11T15:39:00.000Z」を「2023-12-11 15:39」に変換
+func StringToTime2(stringTime string) time.Time {
+	// 文字列を時刻型に変換
+	time, err := time.Parse("2006-01-02T15:04:00.000Z", stringTime)
+	if err != nil {
+		log.Println(err)
+	}
+	return time
 }
