ddb3e098dd414c703a7bf532a7416da662545107
yukke23 <ec98721zzz@gmail.com>
2023-12-20 14:05:45 +0900

    create go teacher api SearchAllDocumentList

--- go/src/main/model/teacher.go
+++ go/src/main/model/teacher.go
@@ -8,6 +8,19 @@ type ReadAllDocumentListResponse struct {
  	UserName     string `json:"user_name"`
 }
 
+// ReadAllDocumentListRequest は、書類一覧を取得する際に必要なデータを保持するための構造体
+type ReadAllDocumentListRequest struct {
+	UserNumber  string `json:"user_number"`
+}
+
+// SearchAllDocumentListRequest は、書類一覧を取得する際に使用する構造体
+type SearchAllDocumentListResponse struct {
+	DocumentID   int    `json:"document_id"`
+	DivisionName string `json:"division_name"`
+	DivisionDetail string `json:"division_detail"`
+ 	UserName     string `json:"user_name"`
+}
+
 // NextAllDocumentRequest は、現在閲覧している書類の前後の書類を取得する際に必要なデータを保持するための構造体
 type NextAllDocumentRequest struct {
 	DocumentID int `json:"document_id"`	//書類ID
