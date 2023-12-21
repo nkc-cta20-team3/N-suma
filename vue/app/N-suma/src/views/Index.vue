0c713907cde35176fd3e60b90b30021f593bb196
yukke23 <ec98721zzz@gmail.com>
2023-12-21 10:31:54 +0900

    update vue index.vue

--- vue/app/N-suma/src/views/Index.vue
+++ vue/app/N-suma/src/views/Index.vue
@@ -2,9 +2,26 @@
   <!-- キービジュアルを置く -->
   <v-container>
     <v-row justify="center">
+      <v-col
+        v-if="store.getIsLogin && !store.getRole"
+        cols="12"
+        sm="10"
+        md="8"
+        lg="6"
+      >
+        <p class="display-1 text-center">
+          あなたのアカウントは利用できません。<br />
+          役職が未割当か、凍結されている可能性があります。<br />
+          管理者に問い合わせてください。
+        </p>
+      </v-col>
       <v-col cols="12" sm="10" md="8" lg="6">
         <v-img src="/Poster.gif" height="100%" width="100%"></v-img>
       </v-col>
     </v-row>
   </v-container>
 </template>
+<script setup>
+import { useStore } from "@/stores/user";
+const store = useStore();
+</script>
